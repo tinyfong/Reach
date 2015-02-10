a4cfbb754e070d8b627edc3cbdb75910a81e2bf0
Tiny <fesly@qq.com>
2015-02-11 00:05:43 +0800

    Normal

---
+++ Reach.AccessYouku.Test/ThumbnailHandlerTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Reach.AccessYouku.Test
+{
+    [TestClass]
+    public class ThumbnailHandlerTest
+    {
+        [TestMethod]
+        public void TestGetBigThumbnail()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
