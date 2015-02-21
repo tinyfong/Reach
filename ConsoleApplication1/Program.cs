f129f5337cfb4b828b9863174050ab98d3d21c30
Tiny <fesly@qq.com>
2015-02-21 14:18:00 +0800

    Normal

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,39 +11,9 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-
-            //using (var db = new BloggingContext())
-            //{
-            //    // Create and save a new Blog 
-            //    Console.Write("Enter a name for a new Blog: ");
-            //    var name = Console.ReadLine();
-
-            //    var blog = new Blog { Name = name };
-            //    db.Blogs.Add(blog);
-            //    db.SaveChanges();
-
-            //    // Display all Blogs from the database 
-            //    var query = from b in db.Blogs
-            //                orderby b.Name
-            //                select b;
-
-            //    Console.WriteLine("All blogs in the database:");
-            //    foreach (var item in query)
-            //    {
-            //        Console.WriteLine(item.Name);
-            //    }
-
-            //    Console.WriteLine("Press any key to exit...");
-            //    Console.ReadKey();
-            //} 
-
-            using (ReachDataContext db = new ReachDataContext())
-            {
-                Movie m = new Movie();
-                m.Name = "A";
-                db.Movies.Add(m);
-                db.SaveChanges();
-            }
+            NewtonJsonTest t = new NewtonJsonTest();
+            t.Run();
+       
         }
     }
 }
--- Reach.AccessYouku.Test/ThumbnailHandlerTest.cs
+++ Reach.AccessYouku.Test/ThumbnailHandlerTest.cs
@@ -1,15 +1,32 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using System.Drawing;
 namespace Reach.AccessYouku.Test
 {
     [TestClass]
     public class ThumbnailHandlerTest
     {
+        string url = "http://v.youku.com/player/getPlayList/VideoIDS/XODg3ODYwODgw";
+
+        [TestMethod]
+        public void TestGetBigThumbnailUrl()
+        {
+
+            string expected = "http://g4.ykimg.com/11270F1F4654DAD3ABB00E000000005A574927-F407-8F61-9BB2-D0D42EC06283";
+
+
+            ThumbnailHandler handler = new ThumbnailHandler();
+            string back = handler.GetBigThumbnailUrl(url);
+
+            Assert.AreEqual<string>(back, expected);
+        }
+
         [TestMethod]
         public void TestGetBigThumbnail()
         {
-            Assert.IsTrue(true);
+            ThumbnailHandler handler = new ThumbnailHandler();
+            var back = handler.GetBigThumbnail(url);
+            Assert.IsNotNull(true);
         }
     }
 }
