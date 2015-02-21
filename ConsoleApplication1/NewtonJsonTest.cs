7a8fa42e5e9f30a3f051a6490948f5044decbe77
Tiny <fesly@qq.com>
2015-02-21 14:18:36 +0800

    1

---
+++ ConsoleApplication1/NewtonJsonTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reach.AccessYouku.Model;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace ConsoleApplication1
+{
+    class NewtonJsonTest
+    {
+        public void Run()
+        {
+            using (WebClient c = new WebClient())
+            {
+                string url = "http://v.youku.com/player/getPlayList/VideoIDS/XODg3ODYwODgw";
+                string s = c.DownloadString(url);
+                Video v = JsonConvert.DeserializeObject<Video>(s);
+                //v.Data.First()["logo"]
+                //http://g4.ykimg.com/11270F1F4654DAD3ABB00E000000005A574927-F407-8F61-9BB2-D0D42EC06283
+            }
+
+        }
+    }
+}
---
+++ Reach.AccessYouku/ThumbnailHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+using Reach.AccessYouku.Model;
+using System.Drawing;
+using System.IO;
+
+namespace Reach.AccessYouku
+{
+    public class ThumbnailHandler
+    {
+        public string GetBigThumbnailUrl(string url)
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.Encoding = Encoding.UTF8;
+                string back = client.DownloadString(url);
+                Video v = JsonConvert.DeserializeObject<Video>(back);
+                return v.Data.First()["logo"].ToString();
+            }
+        }
+
+        public Image GetBigThumbnail(string url)
+        {
+            string imgUrl = this.GetBigThumbnailUrl(url);
+
+            using (WebClient client = new WebClient())
+            {
+                byte[] buffer = client.DownloadData(imgUrl);
+                MemoryStream ms = new MemoryStream(buffer);
+                Image img = Image.FromStream(ms);
+                return img;
+            }
+
+        }
+    }
+}
