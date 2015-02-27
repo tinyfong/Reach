dff3508f36c3c3267b904ecd82f15bf1ed4f0205
Tiny <tinyfong1989@gmail.com>
2015-02-27 23:44:05 +0800

    2

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            NewtonJsonTest t = new NewtonJsonTest();
+            var t = new CodeFirstTest();
             t.Run();
        
         }
