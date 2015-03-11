347259fee13a45293204d0bded3318edfa227c0b
Tiny <tinyfong1989@gmail.com>
2015-03-11 17:57:21 +0800

    2

---
+++ Reach/Controllers/VideoController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Reach.Models;
+using Reach.DAL;
+
+namespace Reach.Controllers
+{
+    public class VideoController : Controller
+    {
+        private VideoContext db = new VideoContext();
+
+        //
+        // GET: /Video/
+
+        public ActionResult Index()
+        {
+            return View(db.Videos.ToList());
+        }
+
+        //
+        // GET: /Video/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Video video = db.Videos.Find(id);
+            if (video == null)
+            {
+                return HttpNotFound();
+            }
+            return View(video);
+        }
+
+        //
+        // GET: /Video/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Video/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Video video)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Videos.Add(video);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(video);
+        }
+
+        //
+        // GET: /Video/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Video video = db.Videos.Find(id);
+            if (video == null)
+            {
+                return HttpNotFound();
+            }
+            return View(video);
+        }
+
+        //
+        // POST: /Video/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Video video)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(video).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(video);
+        }
+
+        //
+        // GET: /Video/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Video video = db.Videos.Find(id);
+            if (video == null)
+            {
+                return HttpNotFound();
+            }
+            return View(video);
+        }
+
+        //
+        // POST: /Video/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Video video = db.Videos.Find(id);
+            db.Videos.Remove(video);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Reach/DAL/VideoContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Reach.Models;
+
+namespace Reach.DAL
+{
+    public class VideoContext : DbContext
+    {
+        public VideoContext()
+            : base("VideoContext")
+        {
+
+        }
+
+        public DbSet<Video> Videos { get; set; }
+
+    }
+}
