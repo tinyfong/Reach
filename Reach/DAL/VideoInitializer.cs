4c0a01f92e56c24177483d5f72617cc2135dd647
Tiny <tinyfong1989@gmail.com>
2015-03-13 17:54:26 +0800

    2

---
+++ Reach/DAL/VideoInitializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Reach.Models;
+
+namespace Reach.DAL
+{
+    public class VideoInitializer : DropCreateDatabaseIfModelChanges<VideoContext>
+    {
+        protected override void Seed(VideoContext context)
+        {
+            var videos = new List<Video> { 
+            new Video{Id=1}
+            };
+
+            videos.ForEach(x => context.Videos.Add(x));
+            context.SaveChanges();
+        }
+    }
+}
