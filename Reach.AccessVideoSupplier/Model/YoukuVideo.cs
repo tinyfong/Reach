e4b982ea3bc917a0026138332a7c2491e0fc3f48
Tiny <tinyfong1989@gmail.com>
2015-03-25 16:23:19 +0800

    2

---
+++ Reach.AccessVideoSupplier/Model/YoukuVideo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reach.AccessVideoSupplier.Model
+{
+   public class YoukuVideo
+    {
+        public Dictionary<string, object>[] Data { get; set; }
+        public Dictionary<string, string> User { get; set; }
+        public Dictionary<string, string> Controller { get; set; }
+    }
+}
---
+++ Reach.AccessVideoSupplier/YoukuThumbnailHandler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Reach.AccessVideoSupplier.Model;
+using Newtonsoft.Json;
+
+namespace Reach.AccessVideoSupplier
+{
+    public class YoukuThumbnailHandler
+    {
+        public Byte[] GetThumbnailByYoukuId(string youkuId)
+        {
+            string url = GetThumbnailUrl(youkuId);
+            return GetImgaeByUrl(url);
+        }
+
+        public Byte[] GetBigThumbnailByYoukuId(string youkuId)
+        {
+            string url = GetBigThumbnailUrl(youkuId);
+            return GetImgaeByUrl(url);
+        }
+
+        public string GetThumbnailUrl(string youkuId)
+        {
+            string url = "http://events.youku.com/global/api/video-thumb.php?vid={0}";
+
+            return string.Format(url, youkuId);
+        }
+
+
+        public string GetBigThumbnailUrl(string youkuId)
+        {
+            string url = string.Format("http://v.youku.com/player/getPlayList/VideoIDS/{0}", youkuId);
+
+            using (WebClient client = new WebClient())
+            {
+                client.Encoding = Encoding.UTF8;
+                string back = client.DownloadString(url);
+                YoukuVideo v = JsonConvert.DeserializeObject<YoukuVideo>(back);
+                return v.Data.First()["logo"].ToString();
+            }
+        }
+
+        public byte[] GetImgaeByUrl(string url)
+        {
+            using (WebClient client = new WebClient())
+            {
+                byte[] buffer = client.DownloadData(url);
+
+                return buffer;
+            }
+        }
+
+    }
+}
