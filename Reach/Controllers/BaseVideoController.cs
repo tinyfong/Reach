05cdcccc4a718f576b3ef6f635e8cab8863f2ebc
Tiny <tinyfong1989@gmail.com>
2015-03-26 23:49:06 +0800

    2

---
+++ Reach/Controllers/BaseVideoController.cs
@@ -0,0 +1,23 @@
+﻿using Reach.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Reach.Models;
+
+namespace Reach.Controllers
+{
+    public class BaseVideoController : Controller
+    {
+        //
+        // GET: /BaseVideo/
+        protected VideoContext db = new VideoContext();
+    
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Reach/Controllers/ShowcaseController.cs
@@ -0,0 +1,24 @@
+﻿using Reach.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Reach.Controllers
+{
+    public class ShowcaseController : BaseVideoController
+    {
+        //
+        // GET: /Showcase/    
+        public ActionResult Index()
+        {
+            var videoList = db.Set<Video>().ToList();
+            ViewBag.VideoList = videoList;
+
+            return View();
+        }
+
+
+    }
+}
