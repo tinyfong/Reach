95d3c8d8d0beabd50c05c8e2094b2f1c4dce3e78
Tiny <tinyfong1989@gmail.com>
2015-04-03 18:06:05 +0800

    2

---
+++ ConsoleApplication1/ValueInjecterTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Omu.ValueInjecter;
+
+namespace ConsoleApplication1
+{
+    public class ValueInjecterTest
+    {
+        public void Run()
+        {
+            A a = new A();
+            a.Id = 1;
+            a.Name = "a";
+            a.AA = "AA";
+            B b = new B();
+
+            b.InjectFrom(a).InjectFrom<Reverse>(a);
+        }
+    }
+
+    class A
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AA { get; set; }
+    }
+
+    class B
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string BB { get; set; }
+    }
+
+    class Reverse : ConventionInjection
+    {
+        protected override bool Match(ConventionInfo c)
+        {
+            return c.SourceProp.Name == "AA" && c.TargetProp.Name == "BB";
+        }
+    }
+}
