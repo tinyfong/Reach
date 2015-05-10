2862e3a707ee396575fa980f9abfaf30cce842e8
Tiny <fesly@qq.com>
2015-05-10 20:41:21 +0800

    1

--- SchedulingCourses/Program.fs
+++ SchedulingCourses/Program.fs
@@ -1,4 +1,4 @@
-﻿    seq {1..100}
-    |>Seq.filter(fun n->n%2<>0)
-    |>Seq.sum
-    |>printfn "sun is %A"
+﻿    let s1 =seq{1..5}
+    let s2=seq{2..2..10}
+
+    if  Seq.forall2(fun x y ->x*2=y)  s1 s2 then printfn "OK"
