5f1cc2c15ec3f903185ddf65a834a50783e04155
TinyFong <tinyfong1989@gmail.com>
2015-05-12 18:04:06 +0800

    2

--- SchedulingCourses/Program.fs
+++ SchedulingCourses/Program.fs
@@ -1,4 +1,10 @@
-﻿    let f x =x*2
-    let g x =x+5
-    let forwardPipeResult= 3|> f |> g
-    //666
+﻿
+    let input=[1..10]
+
+    let join1 : string*string list-> string =System.String.Join
+
+    let curryFormatJoin = FuncConvert.FuncFromTupled join1
+
+    input
+    |> List.map(fun number->string (number))
+    |> curryFormatJoin "*"
