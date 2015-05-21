6d451fb7fc0ba1b3b95d04426f1067493a170081
TinyFong <tinyfong1989@gmail.com>
2015-05-21 18:32:21 +0800

    2

---
+++ ConsoleApplication2/SQLDataConnection1.fs
@@ -0,0 +1,38 @@
+﻿module SQLDataConnection1
+
+#if INTERACTIVE
+#r "System.Data"
+#r "System.Data.Linq"
+#r "FSharp.Data.TypeProviders"
+#endif
+
+open System.Data
+open System.Data.Linq
+open Microsoft.FSharp.Data.TypeProviders
+
+type SqlConnection = SqlDataConnection<ConnectionString = @"Data Source=(localdb)\v11.0;Initial Catalog=Institute;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False", StoredProcedures=false>
+
+let db = SqlConnection.GetDataContext()
+
+let table =query {
+    for r in db.Course do
+    select r
+    }
+    
+let newRecord = new SqlConnection.ServiceTypes.Course(CourseName = "aa")
+
+db.Course.InsertOnSubmit(newRecord)
+try
+    db.DataContext.SubmitChanges()
+    printfn "record added"
+with _ -> printfn "update failed"
+
+db.Course .DeleteOnSubmit(newRecord)
+try
+    db.DataContext.SubmitChanges()
+    printfn "record deleted"
+with _ -> printfn "update failed"
+
+
+//for p in table do 
+//    printfn"%s" p.CourseName
---
+++ SchedulingCourses/File6.fs
@@ -0,0 +1,39 @@
+﻿namespace FSharp.Converters
+
+open System
+open System.Windows
+open System.Windows.Data
+open Microsoft.FSharp.Reflection
+
+[<AutoOpen>]
+module FunctionLibrary =
+    let nullFunction = fun value _ _ _ -> value
+    
+    let stringToInt (a:Object) = Convert.ToInt32(a)
+    let intToBool = fun i -> i = 0
+    let boolToVisibility = fun b ->
+        if b then Visibility.Visible
+        else Visibility.Collapsed
+
+    let convert<'T> f (obj:System.Object) (t:Type) (para:System.Object) (culture:Globalization.CultureInfo) = (obj :?> 'T) |> f |>box
+        
+    [<AbstractClassAttribute>]
+    type ConverterBase(convertFunction, convertBackFunction) =
+        new() = ConverterBase(nullFunction, nullFunction)
+
+        interface IValueConverter with
+            override this.Convert (value, targetType, parameter, culture) =
+                this.Convert value targetType parameter culture
+            override this.ConvertBack(value, targetType, parameter, culture) =
+                this.ConvertBack value targetType parameter culture
+        abstract member Convert : (obj -> Type -> obj -> Globalization.CultureInfo -> obj)
+        default this.Convert = convertFunction
+
+        abstract member ConvertBack : (obj -> Type -> obj -> Globalization.CultureInfo -> obj)
+        default this.ConvertBack = convertBackFunction
+
+    type StringToVisiblityConverter() =
+        inherit ConverterBase(stringToInt >> intToBool >> boolToVisibility |> convert, nullFunction)
+    
+    type DebuggerConverter() =
+        inherit ConverterBase(nullFunction, nullFunction)
