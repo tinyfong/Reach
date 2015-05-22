206767d4f273a61aac0721a2aca8b67f6577c49e
TinyFong <tinyfong1989@gmail.com>
2015-05-22 18:03:57 +0800

    2

--- ConsoleApplication2/SQLDataConnection1.fs
+++ ConsoleApplication2/SQLDataConnection1.fs
@@ -4,35 +4,85 @@
 #r "System.Data"
 #r "System.Data.Linq"
 #r "FSharp.Data.TypeProviders"
+#r "System.Linq"
 #endif
 
 open System.Data
 open System.Data.Linq
+open System.Linq
 open Microsoft.FSharp.Data.TypeProviders
+open Microsoft.FSharp.Linq.NullableOperators
 
 type SqlConnection = SqlDataConnection<ConnectionString = @"Data Source=(localdb)\v11.0;Initial Catalog=Institute;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False", StoredProcedures=false>
 
 let db = SqlConnection.GetDataContext()
 
-let table =query {
-    for r in db.Course do
-    select r
+let internal q0 = query {
+    for n in db.Course do
+    select n.CourseName
     }
-    
-let newRecord = new SqlConnection.ServiceTypes.Course(CourseName = "aa")
 
-db.Course.InsertOnSubmit(newRecord)
-try
-    db.DataContext.SubmitChanges()
-    printfn "record added"
-with _ -> printfn "update failed"
+let internal q1 = query {
+    for n in db.Course.AsEnumerable() do
+    select (sprintf "course name = %s" n.CourseName) }
 
-db.Course .DeleteOnSubmit(newRecord)
-try
-    db.DataContext.SubmitChanges()
-    printfn "record deleted"
-with _ -> printfn "update failed"
+let showResult query = query |> Seq.iter(printfn "%A")
 
+showResult q1
 
-//for p in table do 
-//    printfn"%s" p.CourseName
+let A = [0; 2; 4; 5; 6; 8; 9]
+let B = [1; 3; 5; 7; 8]
+
+query{ 
+    for a in A do
+    for b in B do
+    where (a < b)
+    select (a, b)
+}
+|> Seq.iter(fun (n, e) -> printfn "%d is less than %d" n e)
+
+let internal q2 = query {
+    for n in db.Student do
+    where (n.Name.Length > 4)
+    select n.Name }
+
+showResult q2
+
+let internal q3 = query {
+    for n in db.Student do
+    select n.Name
+    }
+let q3_2 = q3 |> Seq.filter(fun n -> n.Length > 4)
+showResult q3_2
+
+let internal myQuery = query {
+    for n in db.Student do
+    select n.Name}
+
+let result =
+    myQuery
+    |> Seq.mapi(fun i n -> (i, n))
+    |> Seq.filter(fun (i, n) -> i % 2 = 0)
+    |> showResult
+
+let internal q5 = query {
+    for student in db.Student do
+    leftOuterJoin selection in db.CourseSelection on
+        (student.StudentId =? selection.CourseId) into result
+    for selection in result do
+    select (student.Name, selection.Course)
+    }
+
+showResult q5
+
+let q6 = query {
+    for student in db.Student do
+    groupJoin courseSelection in db.CourseSelection
+        on (student.StudentId = courseSelection.StudentId) into g
+    for  courseSelection in g do
+    join course in db.Course on (courseSelection.CourseId ?= course.CourseId)
+    select (student.Name, course.CourseName)
+    }
+
+showResult q6
+
